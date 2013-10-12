aaadd4e21726ebc8d7072f4a4c6fb0e8022081b2
Shriharsh Mishra <harsh82@gmail.com>
2013-10-12 17:24:49 +0530

    Adding some CDI magic.
    Adding some CDI magic. 
    1) Creating a producer for Logger instance which makes it easy to create
    Logger instance in each class. 
    2) Instead of using @PersistenceContext everywhere, an alias is created
    which can be used injected.

--- zipstart/src/main/java/com/sm/mnc/data/RegionRepository.java
+++ zipstart/src/main/java/com/sm/mnc/data/RegionRepository.java
@@ -1,26 +1,31 @@
 package com.sm.mnc.data;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 import com.sm.mnc.model.Region;
 
 @ApplicationScoped
 public class RegionRepository {
 
-	@PersistenceContext
+	@Inject
 	private EntityManager em;
 	
 	public RegionRepository() {		
 	}
 	
-	@SuppressWarnings("unchecked")
+	@Inject
+	private Logger log;
+
+	
 	public List<Region> getAllRegions() {
 		String query = " select r from Region r order by r.regionName ";
-		return em.createQuery(query).getResultList();
+		log.info("JPQL Query for Region - " + query);
+		return em.createQuery(query, Region.class).getResultList();
 	}
 
 }
