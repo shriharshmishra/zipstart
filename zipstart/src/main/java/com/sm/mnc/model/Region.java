6eaa8c32bfbfff7e2d0f340b98f7e253141eefef
Shriharsh Mishra <harsh82@gmail.com>
2013-10-12 18:03:35 +0530

    Adding some validations to our Region entity. Region name is made unique and a unique constraint on the Regions table (and optionally on the Region.java entity in case one wants to create tables from entities)

--- zipstart/src/main/java/com/sm/mnc/model/Region.java
+++ zipstart/src/main/java/com/sm/mnc/model/Region.java
@@ -1,17 +1,24 @@
 package com.sm.mnc.model;
 
 import java.io.Serializable;
-import java.lang.Long;
-import java.lang.String;
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
 
 /**
  * Entity implementation class for Entity: Region
  *
  */
 @Entity
-@Table(name="REGION")
-
+@Table(name="REGIONS", uniqueConstraints = @UniqueConstraint(columnNames = "REGION_NAME"))
 public class Region implements Serializable {
 
 	
@@ -34,7 +41,9 @@ public class Region implements Serializable {
 		this.regionId = regionId;
 	}   
 	
-	@Column(name="REGION_NAME")
+	@NotNull(message = "Region name can't be blank.")
+	@Length(min=3, max=25, message = "Region name should have at least 2 or maximum 25 characters.")
+	@Column(name="REGION_NAME")	
 	public String getRegionName() {
 		return this.regionName;
 	}
