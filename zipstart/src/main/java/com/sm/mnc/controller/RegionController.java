8f5c7f9bec09ea6f04489ed11ad33d7be1bf27a2
Shriharsh Mishra <harsh82@gmail.com>
2013-10-14 21:57:14 +0530

    Okay we made our EJB conversational but didn't use it in a conversation that can be managed by the CDI container. Hence, beginning the conversation in loadRegion method and injecting the EJB using our good old friend @Inject.

--- zipstart/src/main/java/com/sm/mnc/controller/RegionController.java
+++ zipstart/src/main/java/com/sm/mnc/controller/RegionController.java
@@ -2,12 +2,11 @@ package com.sm.mnc.controller;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.enterprise.context.Conversation;
 import javax.enterprise.inject.Model;
-import javax.enterprise.inject.Produces;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
-import javax.inject.Named;
 
 import com.sm.mnc.model.Region;
 import com.sm.mnc.service.RegionService;
@@ -15,14 +14,15 @@ import com.sm.mnc.service.RegionService;
 @Model
 public class RegionController {
 
-	@EJB
+	@Inject
 	private RegionService regionService;
 	
 	@Inject
 	private FacesContext facesContext;
 	
-	@Named
-	@Produces
+	@Inject
+	Conversation conversation;
+	
 	private Region editedRegion;
 	
 	@PostConstruct
@@ -41,8 +41,9 @@ public class RegionController {
 		}
 	}
 	
-	public String loadRegion(Region region) {	
-		editedRegion = region;
+	public String loadRegion(Region region) {
+		conversation.begin();
+		regionService.loadRegion(region);
 		return "viewRegion";
 	}
 
