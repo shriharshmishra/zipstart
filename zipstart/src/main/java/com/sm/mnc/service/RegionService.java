94c1e0f8209f036c48fc3e4ddf0f10d0892625a2
Shriharsh Mishra <harsh82@gmail.com>
2013-10-14 21:55:42 +0530

    So we saw lazy loading exception when tried viewing the countries in a region. Conversations can help us here. So here I'm modifying the our EJB RegionService.java to make it stateful and conversational. Stateful because we can then utilize Extended persistence context. editedRegion bean has been moved from RegionController.java to RegionService.java so that it also becomes conversational.

--- zipstart/src/main/java/com/sm/mnc/service/RegionService.java
+++ zipstart/src/main/java/com/sm/mnc/service/RegionService.java
@@ -1,24 +1,42 @@
 package com.sm.mnc.service;
 
-import javax.ejb.Stateless;
+import javax.ejb.Stateful;
+import javax.enterprise.context.ConversationScoped;
 import javax.enterprise.event.Event;
+import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 
 import com.sm.mnc.model.Region;
 import com.sm.mnc.qualifier.Created;
 
-@Stateless
+@Stateful
+@ConversationScoped
 public class RegionService {
 
-	@Inject
+	@PersistenceContext(type = PersistenceContextType.EXTENDED)
 	EntityManager em;
 	
 	@Inject	@Created Event<Region> regionEvent;
 	
+	Region editedRegion;
+	
+	@Produces
+	@Named
+	public Region getEditedRegion() {
+		return editedRegion;
+	}
+
 	public void saveRegion(Region region) {
 		em.persist(region);
 		regionEvent.fire(region);
+	}
+	
+	public void loadRegion(Region region) {
+		editedRegion = em.find(Region.class, region.getRegionId());
 	}
 
 }
